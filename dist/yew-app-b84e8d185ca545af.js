fa7baa450dabd760a82c7cd26d2b0193f5a5a00f
Forrest Walker <forrestpatwalker@hotmail.com>
2023-04-18 06:11:08 -0600

    added content to the page, created components, and created video data

--- dist/yew-app-b84e8d185ca545af.js
+++ dist/yew-app-b84e8d185ca545af.js
@@ -580,12 +580,12 @@ function getImports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper3422 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 219, __wbg_adapter_16);
+    imports.wbg.__wbindgen_closure_wrapper4058 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 299, __wbg_adapter_16);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper3546 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 232, __wbg_adapter_19);
+    imports.wbg.__wbindgen_closure_wrapper4186 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 312, __wbg_adapter_19);
         return addHeapObject(ret);
     };
 
@@ -623,7 +623,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('yew-app-948384516b402f38_bg.wasm', import.meta.url);
+        input = new URL('yew-app-b84e8d185ca545af_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
